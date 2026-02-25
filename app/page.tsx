9e748d679924c9a93f48ed55f6087702c0307eae
toffly <toffly@outlook.com>
2026-02-25 13:50:22 +0700

    real events

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database";
-import { events } from "@/lib/constants";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
 const page = async () => {
-  // const response = await fetch(`${BASE_URL}/api/events`)
-  // const {events} = await response.json()
+  const response = await fetch(`${BASE_URL}/api/events`)
+  const {events} = await response.json()
 
   return (
     <section>
