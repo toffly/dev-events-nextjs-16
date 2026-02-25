70a2473d65fdd13dcf59006ff82d930a34a9a592
toffly <toffly@outlook.com>
2026-02-25 13:25:58 +0700

    v16

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,153 +1,19 @@
-import BookEvent from "@/components/BookEvent";
-import EventCard from "@/components/EventCard";
-import { IEvent } from "@/database";
-import { getSimilarEventsBySlug } from "@/lib/actions/event.actions";
-import Image from "next/image";
-import { notFound } from "next/navigation";
-
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
-const EventDetailItem = ({
-  icon,
-  alt,
-  label,
-}: {
-  icon: string;
-  alt: string;
-  label: string;
-}) => (
-  <div className="flex-row-gap-2 items-center">
-    <Image src={icon} alt={alt} width={17} height={17} />
-    <p>{label}</p>
-  </div>
-);
-
-const EventAgenda = ({ agendaItems }: { agendaItems: string[] }) => (
-  <div>
-    <h2>Agenda</h2>
-    <ul>
-      {agendaItems.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
-    </ul>
-  </div>
-);
-
-const EventTags = ({ tags }: { tags: string[] }) => (
-  <div className="flex flex-row gap-1.5 flex-wrap">
-    {tags.map((tag) => (
-      <div className="pill" key={tag}>
-        {tag}
-      </div>
-    ))}
-  </div>
-);
+import EventDetails from "@/components/EventDetails";
+import { Suspense } from "react";
 
 const EventDetailsPage = async ({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) => {
-  const { slug } = await params;
-  const response = await fetch(`${BASE_URL}/api/events/${slug}`);
-  const {
-    event: {
-      description,
-      image,
-      overview,
-      date,
-      time,
-      location,
-      mode,
-      agenda,
-      audience,
-      tags,
-      organizer,
-    },
-  } = await response.json();
-
-  if (!description) return notFound();
-
-  const booking = 10;
-  
-  
-  const similarEvents: IEvent[] = await getSimilarEventsBySlug(slug)
+  const slug = params.then((p) => p.slug);
 
   return (
-    <section id="event">
-      <div className="header">
-        <h1>Event Description</h1>
-        <p>{description}</p>
-      </div>
-
-      <div className="details">
-        <div className="content">
-          <Image
-            src={image}
-            alt="Event Banner"
-            width={800}
-            height={800}
-            className="banner"
-          />
-
-          <section className="flex-col-gap-2">
-            <h2>Overview</h2>
-            <p>{overview}</p>
-          </section>
-          <section className="flex-col-gap-2">
-            <h2>Event Details</h2>
-
-            <EventDetailItem
-              icon="/icons/calendar.svg"
-              alt="calendar"
-              label={date}
-            />
-            <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time} />
-            <EventDetailItem icon="/icons/pin.svg" alt="pin" label={location} />
-            <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode} />
-            <EventDetailItem
-              icon="/icons/audience.svg"
-              alt="audience"
-              label={audience}
-            />
-          </section>
-
-          <EventAgenda agendaItems={JSON.parse(agenda[0])} />
-          <section className="flex-col-gap2">
-            <h2>About the Organizer</h2>
-            <p>{organizer}</p>
-          </section>
-
-          <EventTags tags={JSON.parse(tags[0])} />
-        </div>
-
-        <aside className="booking">
-          <div className="signup-card">
-            <h2>Book Your Spot</h2>
-            {booking > 0 ? (
-              <p className="text-sm">
-                Join {booking} people who have already their spot!
-              </p>
-            ) : (
-              <p className="text-sm">Be the first to book your spot!</p>
-            )}
-
-            <BookEvent/>
-          </div>
-        </aside>
-      </div>
-
-      <div className="flex w-full flex-col gap-4 pt-20">
-        <h2>Similar Events</h2>
-        <div className="event">
-          {similarEvents.length > 0 && similarEvents.map((similarEvents: IEvent) => (
-            <EventCard key={similarEvents.title} {...similarEvents} />
-          ))}
-
-        </div>
-
-      </div>
-    </section>
+    <main>
+      <Suspense fallback={<div>Loading ...</div>}>
+      <EventDetails params={slug}/>
+      </Suspense>
+    </main>
   );
 };
 
